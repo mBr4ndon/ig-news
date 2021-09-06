0d928c225d57860f0439a159604f528448b31fd5
Márcio Brandão <brandao.marcio.xix@gmail.com>
2021-09-06 19:44:53 +0100

    Introdução do sistema de login com Github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from '../styles/pages/home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
+  console.log(product);
   return (
     <>
       <Head>
@@ -15,12 +26,32 @@ export default function Home() {
           <h1>News about <br/>the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br/> 
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl react" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JVxPDBzEwJRBqIyMpRV8q3r');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-PT', {
+      style: 'currency',
+      currency: 'EUR'
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 1d
+  }
 }
